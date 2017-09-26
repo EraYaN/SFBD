691dca459074f5e00313e916ab990a49230d490f
Robin Hes <robinhes@outlook.com>
2017-09-26 11:49:24 +0200

    Added unix shell scripts to run the program.

--- lab1/ex1/run.sh
+++ lab1/ex1/run.sh
@@ -1,1 +1,1 @@
-$SPARK_HOME/bin/spark-submit --class "WordFreqCounts" --master local[*] target/scala-2.11/WordFreqCounts_2.11-0.1-SNAPSHOT.jar $1
+spark-submit --class "WordFreqCounts" --master local[*] target/scala-2.11/WordFreqCounts_2.11-0.1-SNAPSHOT.jar $1
