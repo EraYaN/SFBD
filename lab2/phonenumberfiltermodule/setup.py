09b03a0cffc1175448a5a3fb7ab840271fbfb0d9
Robin Hes <robinhes@outlook.com>
2017-10-08 13:14:27 +0200

    setup.py now using Homebrew zlib on macOS.

--- lab2/phonenumberfiltermodule/setup.py
+++ lab2/phonenumberfiltermodule/setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup, Extension
+import sys
 
-module1 = Extension('phonenumberfilter',
-                    sources = ['phonenumberfiltermodule.cpp'],
-                    include_dirs = ['../zlib-1.2.11/','../gzstream/'],
-                    libraries = ['zlibstat'],
-                    library_dirs = ['../zlib-1.2.11/contrib/vstudio/vc14/x64/ZlibStatRelease'])
+if sys.platform == 'darwin':
+    module1 = Extension('phonenumberfilter',
+                    sources=['phonenumberfiltermodule.cpp'],
+                    include_dirs=['/usr/local/Cellar/zlib/1.2.11/include', '../gzstream/'],
+                    libraries=['z'],
+                    library_dirs=['/usr/local/Cellar/zlib/1.2.11/lib'])
+else:
+    module1 = Extension('phonenumberfilter',
+                    sources=['phonenumberfiltermodule.cpp'],
+                    include_dirs=['../zlib-1.2.11/', '../gzstream/'],
+                    libraries=['zlibstat'],
+                    library_dirs=['../zlib-1.2.11/contrib/vstudio/vc14/x64/ZlibStatRelease'])
 
-setup (name = 'PhoneNumberFilter',
-       version = '1.1',
-       description = 'This is a package to quickly filter dutch phone numbers from a WARC WET file, now with compression support.',
-       ext_modules = [module1])
+setup (name='PhoneNumberFilter',
+       version='1.1',
+       description='This is a package to quickly filter dutch phone numbers from a WARC WET file, now with compression support.',
+       ext_modules=[module1])
