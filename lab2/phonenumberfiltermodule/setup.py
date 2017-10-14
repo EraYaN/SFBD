aa42927dc96c4f49e57dec40b72594c0ab614977
Robin Hes <robinhes@outlook.com>
2017-10-14 09:33:58 +0200

    Updated files for EC2 bootstrapping.

--- lab2/phonenumberfiltermodule/setup.py
+++ lab2/phonenumberfiltermodule/setup.py
@@ -7,12 +7,18 @@ if sys.platform == 'darwin':
                     include_dirs=['/usr/local/Cellar/zlib/1.2.11/include', '../gzstream/'],
                     libraries=['z'],
                     library_dirs=['/usr/local/Cellar/zlib/1.2.11/lib'])
-else:
+elif sys.platform == 'win32':
     module1 = Extension('phonenumberfilter',
                     sources=['phonenumberfiltermodule.cpp'],
                     include_dirs=['../zlib-1.2.11/', '../gzstream/'],
                     libraries=['zlibstat'],
                     library_dirs=['../zlib-1.2.11/contrib/vstudio/vc14/x64/ZlibStatRelease'])
+else:
+    module1 = Extension('phonenumberfilter',
+                    sources=['phonenumberfiltermodule.cpp'],
+                    include_dirs=['gzstream/'],
+                    libraries=['z'],
+                    library_dirs=[])
 
 setup (name='PhoneNumberFilter',
        version='1.1',
