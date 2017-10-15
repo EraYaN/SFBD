da6f7593042759493f7e5e168796c2ec9d815b84
Erwin de Haan <EraYaN@users.noreply.github.com>
2017-10-15 13:37:27 +0200

    Fixed timing, logging and profiling.

--- lab2/phone_number_analysis/phone_number_analysis.py
+++ lab2/phone_number_analysis/phone_number_analysis.py
@@ -14,10 +14,13 @@ from tempfile import NamedTemporaryFile
 import boto3
 import botocore
 
+import logging
+
 import phonenumberfilter as pnf
 
 
 class PhoneNumbers:
+    s3pattern = re.compile('^s3://([^/]+)/(.+)')
 
     output_schema = StructType([
         StructField("num", StringType(), True),
@@ -31,7 +34,7 @@ class PhoneNumbers:
         self.output_dir = output_dir
         self.partitions = partitions
         self.local = local
-
+        self.logger = None
 
     def run(self):
         if self.local:
@@ -44,36 +47,41 @@ class PhoneNumbers:
             sc = SparkContext(appName=self.name, conf=conf)
         else:
             sc = SparkContext(appName=self.name)
-
-        self.failed_record_parse = sc.accumulator(0)
+        
+        #sc.setLogLevel('info')
         self.failed_segment = sc.accumulator(0)
+        self.download_time = sc.accumulator(0.0)
+        self.process_time = sc.accumulator(0.0)
 
         sqlc = SQLContext(sparkContext=sc)
 
         if self.partitions is None:
             self.partitions = sc.defaultParallelism
 
-        self.log(sc, "Started..")
-        t0 = time.time()
+        
+        self.log(sc,"Started...".format(self.partitions))       
+        t0 = time.perf_counter()
         input_data = sc.textFile(self.input_file, minPartitions=self.partitions)
-
-        phone_numbers = input_data.flatMap(self.process_warcs).flatMap(lambda x: x)
-
+        segments = input_data.count()
+        usedpartitions = input_data.getNumPartitions()
+        self.log(sc,"Data has {} segments on {} partitions..".format(segments, usedpartitions))       
+        phone_numbers = input_data.flatMap(self.process_warcs)        
         phone_numb_agg_web = phone_numbers.groupByKey().mapValues(list)
-
+        
         sqlc.createDataFrame(phone_numb_agg_web, schema=self.output_schema) \
                 .write \
                 .mode('overwrite') \
                 .format("parquet") \
                 .save(self.output_dir)
 
-        t1 = time.time()
-        self.log(sc, "Found {} unique phone numbers in total.".format(phone_numb_agg_web.count()))
-        self.log(sc, "New implementation took: {:.3f} seconds.".format(t1-t0))
-        t2 = time.time()
+        t1 = time.perf_counter()
 
-        self.log(sc, "Failed segments: {}".format(self.failed_segment.value))
-        self.log(sc, "Failed parses: {}".format(self.failed_record_parse.value))
+        self.log(sc,"Found {} unique phone numbers in total.".format(phone_numb_agg_web.count()))
+        self.log(sc,"New implementation took: {:.3f} seconds.".format(t1-t0))
+        self.log(sc,"Download took: {0:.3f} seconds or {1:.3f} seconds per partition and {2:.3f} per segement.".format(self.download_time.value, self.download_time.value/usedpartitions, self.download_time.value/segments))
+        self.log(sc,"Processing took: {0:.3f} seconds or {1:.3f} seconds per partition and {2:.3f} per segement.".format(self.process_time.value, self.process_time.value/usedpartitions, self.process_time.value/segments))
+        self.log(sc,"Processed segments: {}".format(segments-self.failed_segment.value))
+        self.log(sc,"Failed segments: {}".format(self.failed_segment.value))
 
 
     def log(self, sc, message, level="warn"):
@@ -87,40 +95,35 @@ class PhoneNumbers:
 
 
     def process_warcs(self, input_uri):
-        t_start = time.time()
+        t_start = time.perf_counter()
 
         if input_uri.startswith('file:'):
+            t_mid = t_start
             res = self.process_records(input_uri[5:], False)
-            t_end = time.time()
-            print("##Download: {0};{1:.3f}".format(input_uri[5:], 0))
-            print("##Process: {0};{1:.3f}".format(input_uri[5:], t_end - t_start))
+            t_end = time.perf_counter()            
 
         elif input_uri.startswith('s3:/'):
             tempfileobj = self.process_s3_warc(input_uri)
             tempname = tempfileobj.name
             tempfileobj.close()
-            print("Passing {} temp file to the C code.".format(tempname))
-
-            t_mid = time.time()
+            
             res = self.process_records(tempname, True)
-            t_end = time.time()
-
-            print("##Download: {0};{1:.3f}".format(input_uri[5:], t_mid - t_start))
-            print("##Process: {0};{1:.3f}".format(input_uri[5:], t_end - t_mid))
+            t_end = time.perf_counter()            
         else:
             res = []
 
+        self.download_time.add(t_mid - t_start)
+        self.process_time.add(t_end - t_mid)
         return res
 
 
     def process_s3_warc(self, uri):
         try:
             no_sign_request = botocore.client.Config(signature_version=botocore.UNSIGNED)
-            s3client = boto3.client('s3', config=no_sign_request)
-            s3pattern = re.compile('^s3://([^/]+)/(.+)')
-            s3match = s3pattern.match(uri)
+            s3client = boto3.client('s3', config=no_sign_request)            
+            s3match = self.s3pattern.match(uri)
             if s3match is None:
-                # self.log("Invalid URI: {}".format(uri), level='error')
+                print("ERROR: Invalid URI: {}".format(uri))
                 self.failed_segment.add(1)
                 return None
             bucketname = s3match.group(1)
@@ -130,7 +133,7 @@ class PhoneNumbers:
 
             return warctemp
         except BaseException as e:
-            # self.log("Failed fetching {}\nError: {}".format(uri, e), level='error')
+            print("ERROR: Failed fetching {}\nError: {}".format(uri, e))
             self.failed_segment.add(1)
 
             return None
@@ -139,12 +142,12 @@ class PhoneNumbers:
     def process_records(self, filename, is_s3=False):
         try:
             if is_s3:
-                yield pnf.load(filename, True, True)
+                return pnf.load(filename, True, True)
             else:
-                yield pnf.load(filename, True, False)
+                return pnf.load(filename, True, False)
 
         except BaseException as e:
-            # self.log("Failed parsing with C implementation.\nError: {}".format(e), level='error')
+            print("ERROR: Failed parsing with C implementation.\nError: {}".format(e))
             self.failed_segment.add(1)
 
 
